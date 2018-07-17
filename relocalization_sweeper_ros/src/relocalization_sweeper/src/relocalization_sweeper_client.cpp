0ea186adf9ba378caf4dc0a1cba605f2c9814ed3
wzmBeiJing <smile6228@126.com>
2018-07-17 15:14:22 +0800

    relocalization robot sweeper

--- relocalization_sweeper_ros/src/relocalization_sweeper/src/relocalization_sweeper_client.cpp
+++ relocalization_sweeper_ros/src/relocalization_sweeper/src/relocalization_sweeper_client.cpp
@@ -16,32 +16,45 @@
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <boost/thread/thread.hpp>
 #include <boost/make_shared.hpp>
 
 #include <stdio.h>
 #include <math.h>
-
-#include <hash_set>
-#include <set>
-#include <map>
 #include <algorithm> 
-
 #include <tf/tf.h>
+#include <string>
 #include "relocalization_sweeper/GetRobotPose.h"
 
+using namespace cv;
+using namespace std;
 
+std::string data_dir;
 int main(int argc,char **argv){
     
     ros::init(argc, argv, "relocalization_robot_client");
 
-    ros::NodeHandle n;
+    ros::NodeHandle n,pn("~");
+    pn.param<std::string>("data_dir",data_dir,"../data/");
+    std::string image_dir = data_dir + "1_rgb.png";
+    cv::Mat test = imread(image_dir);
 
     ros::ServiceClient client = n.serviceClient<relocalization_sweeper::GetRobotPose>("/sweeper/relocalization_robot_srv");
     relocalization_sweeper::GetRobotPose srv;
-    
-    srv.request.img_data = sensor_msgs::Image();
+   
+    ros::Time time=ros::Time::now();  
+    cv_bridge::CvImage cvi;
+    cvi.header.stamp = time;
+    cvi.header.frame_id = "image";
+    cvi.encoding = "mono8";
+    cvi.image = test;
+     
+    sensor_msgs::Image im;
+    cvi.toImageMsg(im);
+
+    srv.request.img_data = im;
     
     if(client.call(srv)){
         ROS_INFO("Relocalization Robot Success...");
--- relocalization_sweeper_ros/src/relocalization_sweeper/src/relocalization_sweeper_node.cpp
+++ relocalization_sweeper_ros/src/relocalization_sweeper/src/relocalization_sweeper_node.cpp
@@ -1,6 +1,7 @@
-#include <deque>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "DBoW3/DBoW3.h"
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/matx.hpp>
@@ -16,21 +17,19 @@
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <boost/thread/thread.hpp>
 #include <boost/make_shared.hpp>
-
 #include <stdio.h>
 #include <math.h>
-
-#include <hash_set>
-#include <set>
-#include <map>
 #include <algorithm> 
 #include <tf/tf.h>
+#include <string>
+#include <vector>
 #include "relocalization_sweeper/GetRobotPose.h"
 
-
+std::string voc_dir;
 namespace relocalization_robot{
 using namespace __gnu_cxx;
 using namespace std;
@@ -44,6 +43,26 @@ bool GetRobotCurrentPose(
                          relocalization_sweeper::GetRobotPose::Request &req,
                          relocalization_sweeper::GetRobotPose::Response &res){
     ROS_INFO("start relocalization algorithm...");
+    ROS_INFO("Load vocabulary...");
+    
+    ROS_INFO("Vocabulary Direction:%s",voc_dir.c_str());
+    DBoW3::Vocabulary voc(voc_dir+"voc.yml.gz");
+    //std::cout << __FILE__ << __LINE__ << std::endl;
+
+    if(voc.empty()){
+        ROS_ERROR("Load vocabulary fail...");
+        return false;
+    }
+
+    //std::cout << __FILE__ << __LINE__ << std::endl;
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr =  cv_bridge::toCvCopy(req.img_data, sensor_msgs::image_encodings::MONO8);
+
+    //std::cout << __FILE__ << __LINE__ << std::endl;
+    cv::Mat source = cv_ptr->image;
+    ROS_INFO("Image Height:%d,Image Width:%d",source.rows,source.cols);
+
+    //std::cout << __FILE__ << __LINE__ << std::endl;
     return true;
 }
 
@@ -51,11 +70,12 @@ bool GetRobotCurrentPose(
 
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "relocalization_robot_srv");
+  ros::init(argc, argv, "relocalization_robot_service");
 
   ros::NodeHandle private_nh("~");
+  private_nh.param<std::string>("voc_dir",voc_dir,"../voc/");
 
-  //  advertise a service for getting a coverage plan
+  //advertise a service for getting a coverage plan
   ros::ServiceServer make_coverage_plan_srv = private_nh.advertiseService(
       "/sweeper/relocalization_robot_srv",
       relocalization_robot::GetRobotCurrentPose);
